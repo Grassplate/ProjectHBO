923cfa6dd15df36ff1ec3834d6879c367c7dfbe8
Grassplate <64741498+Grassplate@users.noreply.github.com>
2024-11-14 20:40:50 +0100

    Versie 0.0.7

--- Script.ps1
+++ Script.ps1
@@ -2,13 +2,19 @@
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 
 #Variabelen
-$vmdk = "https://sourceforge.net/projects/osboxes/files/v/vm/59-Uu--svr/24.04/64bit.7z/download"
-$vmdkdownload = 'C:\Users\Codero Admin\Documents\GitHub\ProjectHBO\Bestanden\64bit.7z'
+$vmdk = "https://www.dropbox.com/scl/fi/b23m07lcqkn0k62lfs2n8/Ubuntu-Server-24.04-64bit.zip?rlkey=pym8ux2n2ojqjuuy0man7du2r&st=7mr74day&dl=1"
+$vmdkdownload = 'C:\Users\Codero Admin\Documents\GitHub\ProjectHBO\Bestanden\Ubuntu-Server-24.04-64bit.zip'
 $folder = "C:\Users\Codero Admin\Documents\GitHub\ProjectHBO"
 $bestanden = "C:\Users\Codero Admin\Documents\GitHub\ProjectHBO\Bestanden"
 $Variable = Get-Variable
 Clear-Host
 
+#Folder 'Bestanden' aanmaken
+Write-Host Folder genaamd Bestanden aanmaken. -ForegroundColor Cyan 
 New-Item -Path $folder -Name "Bestanden" -ItemType "directory"
+#Ubuntu Server.zip downloaden
+Write-Host Ubuntu Server.zip downloaden naar Bestanden. Dit kan enkele minuten duren afhankelijk van de internetsnelheid. -ForegroundColor Cyan 
 Invoke-WebRequest $vmdk -OutFile $vmdkdownload
+#Ubuntu Server.zip uitpakken
+Write-Host Ubuntu Server.zip uitpakken in Bestanden. -ForegroundColor Cyan 
 Expand-Archive -LiteralPath $vmdkdownload -DestinationPath $bestanden
