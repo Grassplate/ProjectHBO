84709b410695cb2cc57f3648d746cf4dce478af4
Grassplate <64741498+Grassplate@users.noreply.github.com>
2024-11-14 14:23:03 +0100

    Update Script.ps1
    Versie 0.0.4

--- Script.ps1
+++ Script.ps1
@@ -1,10 +1,11 @@
 #Variabelen
 $vmdk = "https://techloudgeek.com/download/image/?link=https://dlconusc1.linuxvmimages.com/046389e06777452db2ccf9a32efa3760:vmware/U/24.04/UbuntuServer_24.04_VM.7z"
 $vmdkdownload = 'C:\Users\Floris-Codero\Documents\GitHub\ProjectHBO\Bestanden\UbuntuServer_24.04_VM.7z'
-$bestanden = 'C:\Users\Floris-Codero\Documents\GitHub\ProjectHBO\Bestanden'
+$folder = "C:\Users\Floris-Codero\Documents\GitHub\ProjectHBO"
+$bestanden = "C:\Users\Floris-Codero\Documents\GitHub\ProjectHBO\Bestanden"
 $Variable = Get-Variable
 Clear-Host
 
-
+New-Item -Path $folder -Name "Bestanden" -ItemType "directory"
 Invoke-WebRequest $vmdk -OutFile $bestanden
 Expand-Archive -LiteralPath $vmdkdownload -DestinationPath $bestanden
