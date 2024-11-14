c15fe0cdf3481cb3be2fccbd771a1372267669b7
Grassplate <64741498+Grassplate@users.noreply.github.com>
2024-11-14 18:00:12 +0100

    Update Script.ps1

--- Script.ps1
+++ Script.ps1
@@ -1,8 +1,8 @@
 #Variabelen
-$vmdk = "https://techloudgeek.com/download/image/?link=https://dlconusc1.linuxvmimages.com/046389e06777452db2ccf9a32efa3760:vmware/U/24.04/UbuntuServer_24.04_VM.7z"
-$vmdkdownload = 'C:\Users\Floris-Codero\Documents\GitHub\ProjectHBO\Bestanden\UbuntuServer_24.04_VM.7z'
-$folder = "C:\Users\Floris-Codero\Documents\GitHub\ProjectHBO"
-$bestanden = "C:\Users\Floris-Codero\Documents\GitHub\ProjectHBO\Bestanden"
+$vmdk = "https://sourceforge.net/projects/osboxes/files/v/vm/59-Uu--svr/24.04/64bit.7z/download"
+$vmdkdownload = 'C:\Users\Codero Admin\Documents\GitHub\ProjectHBO\Bestanden\64bit.7z'
+$folder = "C:\Users\Codero Admi\Documents\GitHub\ProjectHBO"
+$bestanden = "C:\Users\Codero Admi\Documents\GitHub\ProjectHBO\Bestanden"
 $Variable = Get-Variable
 Clear-Host
 
