528c66f5c5e337c9a7e75b8043e47f76563d97e7
Grassplate <64741498+Grassplate@users.noreply.github.com>
2024-11-14 14:18:43 +0100

    Update Script.ps1
    Versie 0.0.3

--- Script.ps1
+++ Script.ps1
@@ -1,7 +1,7 @@
 #Variabelen
 $vmdk = "https://techloudgeek.com/download/image/?link=https://dlconusc1.linuxvmimages.com/046389e06777452db2ccf9a32efa3760:vmware/U/24.04/UbuntuServer_24.04_VM.7z"
 $vmdkdownload = 'C:\Users\Floris-Codero\Documents\GitHub\ProjectHBO\Bestanden\UbuntuServer_24.04_VM.7z'
-$bestanden = "C:\Users\Floris-Codero\Documents\GitHub\ProjectHBO\Bestanden"
+$bestanden = 'C:\Users\Floris-Codero\Documents\GitHub\ProjectHBO\Bestanden'
 $Variable = Get-Variable
 Clear-Host
 
