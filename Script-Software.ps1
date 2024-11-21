9c57c342ddebe0a6eb2132686a33ca66452cb6db
Grassplate <64741498+Grassplate@users.noreply.github.com>
2024-11-21 22:38:08 +0100

    Update Script-Software.ps1

--- Script-Software.ps1
+++ Script-Software.ps1
@@ -1,9 +1,9 @@
 #Geef toestemming script met GitHub te delen (voer handmatig uit)
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 
-$Notepad++ = "https://www.dropbox.com/scl/fi/eda4nmxzz4qoj3sjv0zdl/npp.8.7.Installer.x64.exe?rlkey=lkqc8qsc00ncjhzner3p9f3z6&st=jmhd7ge5&dl=1"
+$Notepad = "https://www.dropbox.com/scl/fi/eda4nmxzz4qoj3sjv0zdl/npp.8.7.Installer.x64.exe?rlkey=lkqc8qsc00ncjhzner3p9f3z6&st=jmhd7ge5&dl=1"
 $VMware = "https://www.dropbox.com/scl/fi/j9mn9suylmbfg088oonnx/VMware-workstation-full-17.6.1-24319023.exe?rlkey=24vkoyh6bwl7smm94eie736ly&st=gdu4f0ek&dl=1"
-$Notepad++Download = 'C:\Users\Codero Admin\Documents\GitHub\ProjectHBO\Bestanden\npp.8.7.Installer.x64.exe'
+$NotepadDownload = 'C:\Users\Codero Admin\Documents\GitHub\ProjectHBO\Bestanden\npp.8.7.Installer.x64.exe'
 $VMwareDownload = 'C:\Users\Codero Admin\Documents\GitHub\ProjectHBO\Bestanden\VMware-workstation-full-17.6.1-24319023.exe'
 
 $folder = "C:\Users\Codero Admin\Documents\GitHub\ProjectHBO"
@@ -14,7 +14,7 @@ Clear-Host
 #Notepad++ downloaden
 Write-Host Notepad++ downloaden naar Bestanden. -ForegroundColor Cyan 
 Start-Sleep 2
-Invoke-WebRequest $Notepad++ -OutFile $Notepad++Download
+Invoke-WebRequest $Notepad -OutFile $NotepadDownload
 
 #VMware downloaden
 Write-Host VMware downloaden naar Bestanden. -ForegroundColor Cyan 
@@ -26,7 +26,7 @@ Invoke-WebRequest $VMware -OutFile $VMwareDownload
 #Notepad++ installeren
 Write-Host Notepad++ installatie wizzard starten. -ForegroundColor Cyan 
 Start-Sleep 2
-Start-Process -FilePath $Notepad++Download
+Start-Process -FilePath $NotepadDownload
 
 #VMware installeren
 Write-Host VMware installatie wizzard starten. -ForegroundColor Cyan 
