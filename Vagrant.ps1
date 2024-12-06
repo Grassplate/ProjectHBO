ff46ee1eb0e502620ebdf98d350dea1c1b140d13
NotReallyTK <Kapper.thomas0605@gmail.com>
2024-12-06 01:59:27 -0800

    Create Vagrant.ps1

---
+++ Vagrant.ps1
@@ -0,0 +1,13 @@
+﻿# Define the URLs
+$vagrantUrl = "https://releases.hashicorp.com/vagrant/2.4.1/vagrant_2.4.1_windows_amd64.msi"
+$vmwareUtilityUrl = "https://releases.hashicorp.com/vagrant-vmware-utility/1.0.23/vagrant-vmware-utility_1.0.23_windows_amd64.msi"
+
+# Define the output paths
+$vagrantOutput = "C:\Path\To\Save\vagrant_2.4.1_windows_amd64.msi"
+$vmwareUtilityOutput = "C:\Path\To\Save\vagrant-vmware-utility_1.0.23_windows_amd64.msi"
+
+# Download the files
+Invoke-WebRequest -Uri $vagrantUrl -OutFile $vagrantOutput
+Invoke-WebRequest -Uri $vmwareUtilityUrl -OutFile $vmwareUtilityOutput
+
+Write-Host "Downloads completed!"
